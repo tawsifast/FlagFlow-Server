ec3b4df7d1c2509a01158ab09ab6c8219afc2449
tawsifast <0070tawsif@gmail.com>
2026-09-17 21:15:32 +0600

    app.ts and server.ts added

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+app.use(cors({origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
-  })
+})
 );
 app.use(express.json());
 
